f43887cc0f7e51cb70d40afe33582031899d76d3
PAkhtars <akhtararifaParveen@gmail.com>
2026-08-21 16:38:05 +0530

    Get Bulk Voters

--- EmployeeAPI/Core/DTOs/VoterDetailsDto.cs
+++ EmployeeAPI/Core/DTOs/VoterDetailsDto.cs
@@ -3,8 +3,8 @@ namespace EmployeeAPI.Core.DTOs
     public class VoterDetailsDto
     {
         public int Id { get; set; }
-        public int AreaNumber { get; set; }
-        public int PartNumber { get; set; }
+        public string? AreaNumber { get; set; }
+        public string? PartNumber { get; set; }
         public string? IsMuslim { get; set; }
         public string? CleanEnglishName { get; set; }
         public string? CleanEnglishRelativeName { get; set; }
